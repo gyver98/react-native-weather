56a860a8550c108d150c2c3652e3ceb710d35659
somdari <matthewchoi7@gmail.com>
2017-11-02 21:22:53 +1100

    first commit

---
+++ App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component {
+  state = {
+    isLoaded: false
+  };
+  
+  render() {
+    const { isLoaded } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoaded ? null: (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the weather data</Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25,
+    paddingRight: 10
+
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100,
+    
+  }
+});
