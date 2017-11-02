718678a50eeda38a045302ee37b7b37b9814caa3
somdari <matthewchoi7@gmail.com>
2017-11-02 21:52:25 +1100

    react-native-weather Weather comp

--- Weather.js
+++ Weather.js
@@ -5,15 +5,46 @@ import { LinearGradient } from 'expo';
 export default class Weather extends Component {
   render() {
     return (
-      <LinearGradient 
-        colors={["#00C6FB", "#005BEA"]} 
-        style={styles.container}
-      />
-    )
+      <LinearGradient
+        colors={["#00C6FB", "#005BEA"]}
+        style={styles.container}>
+        <View style={styles.upper}>
+          <Text>Icon here</Text>
+          <Text>Temp here</Text>
+        </View>
+        <View style={styles.lower}>
+          <Text style={styles.title}>heavy raining</Text>
+          <Text style={styles.subtitle}>For more info look outside</Text>
+        </View>
+      </LinearGradient>
+    );
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  upper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  lower: {
+    flex: 1,
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  title: {
+    fontSize: 38,
+    backgroundColor: "transparent",
+    color: "white",
+    marginBottom: 10
+  },
+  subtitle: {
+    fontSize: 24,
+    backgroundColor: "transparent",
+    color: "white",
+    marginBottom: 24
   }
 })
