d27f9aabc8c39a0e312d5fd874ddbead77708410
somdari <matthewchoi7@gmail.com>
2017-11-02 22:12:56 +1100

    react-native-weather Weather comp

--- Weather.js
+++ Weather.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class Weather extends Component {
   render() {
@@ -9,7 +10,7 @@ export default class Weather extends Component {
         colors={["#00C6FB", "#005BEA"]}
         style={styles.container}>
         <View style={styles.upper}>
-          <Text>Icon here</Text>
+          <Ionicons color="white" size={144} name="ios-rainy" />
           <Text style={styles.temp}>36</Text>
         </View>
         <View style={styles.lower}>
@@ -27,7 +28,8 @@ const styles = StyleSheet.create({
   upper: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    backgroundColor: "transparent"
   },
   temp: {
     fontSize: 48,
